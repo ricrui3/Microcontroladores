d3ce35d13812550c997c0f07bb67121ff24e971d
ricrui3 <ricrui3@gmail.com>
2016-06-05 20:51:51 -0500

    practica 5 en C

--- c/pract05/pract05/main.c
+++ c/pract05/pract05/main.c
@@ -5,13 +5,15 @@
  */ 
 
 #include <avr/io.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <avr/sfr_defs.h> // Para la macro _BV()
 
 
-int main(void)
-{
-	int portBValue = 0x00;
-	bool upDown = true;					//true = up, false = down
+ int main(void)
+ {
+ 	int portBValue = 0x00;
+	int upDown = 1;						//1 = up, 0 = down
 	DDRB = 0xFF;						//Define DDRB como salidas
 	PORTB = portBValue;					//Se define el valor del puerto B en 00
 	DDRD = 0x80;						//Se define el pin 8 de PORTD como salida (PWM)
@@ -27,20 +29,22 @@ int main(void)
 		if((TIFR & _BV(TOV1))){
 			TIFR |= _BV(TOV1);			//Limpia la bandera
 			PORTB = portBValue;			//Muestra el valor en los LEDS
-			if (upDown)
+			if (upDown == 1)
 			{
-							if(portBValue == 0xFF)		//Si el valor es igual a 255
-							portBValue = 0x00;		//	regresar a 0
-							else                        //De otra forma
-							portBValue++;			//Incrementa el valor
+				if(portBValue == 0xFF)		//Si el valor es igual a 255
+					upDown = 0;		//	regresar a 0
+				else                        //De otra forma
+					portBValue++;			//Incrementa el valor
 			} 
 			else
 			{
+				if(portBValue == 0x00)		//Si el valor es igual a 255
+					upDown = 1;		//	regresar a 0
+				else                        //De otra forma
+					portBValue--;			//Incrementa el valor
 			}
+			OCR2 = portBValue;					//Lee el valor del PINA (minidip) y lo guarda en
+											//	OCR2 (PWM)	
 		}
-		PORTB = PINA;					//Lee el valor del PINA (minidip) y lo muestra en
-										//	en PORTB (LEDS)
-		OCR2 = PINA;					//Lee el valor del PINA (minidip) y lo guarda en
-										//	OCR2 (PWM)
 	}
 }
